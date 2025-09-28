6ba7bc65c17bef65bde81e1d3e42308bc01a7319
Matvey <kazancev_md@mail.ru>
2025-09-28 18:43:25 +0300

    исправил вывод, сделал два таргета ./build/arc_cache.x появился

--- source/main.cpp
+++ source/main.cpp
@@ -11,29 +11,27 @@ int SlowGetPage(int key);
 int main(int argc, char* argv[])
 {
     std::ifstream stream_in(argv[1]);
-
     size_t size_cache = 0;
     size_t number_of_pages = 0;
     stream_in >> size_cache >> number_of_pages;
-
     std::vector<int> pages(number_of_pages);
-
     for (size_t i = 0; i < number_of_pages; i++) { stream_in >> pages[i]; }
 
-    CacheARC<int, int> cache_arc(size_cache/2 + size_cache % 2, size_cache/2);
-    CacheBelady<int, int> cache_belady(size_cache, pages);
+    size_t cache_hit = 0;
 
-    size_t cache_hit_arc    = 0;
-    size_t cache_hit_belady = 0;
-
-    for (size_t i = 0; i < number_of_pages; i++)
-    {
-        cache_hit_arc += cache_arc.LookupUpdate(pages[i], SlowGetPage);
-        cache_hit_belady += cache_belady.LookupUpdate(pages[i], SlowGetPage);
-    }
-
-    std::cout << cache_hit_arc    << "\n";
-    std::cout << cache_hit_belady << "\n";
+    #ifdef USE_ARC
+        CacheARC<int, int> cache_arc(size_cache/2 + size_cache % 2, size_cache/2);
+        for (size_t i = 0; i < number_of_pages; i++)
+            cache_hit += cache_arc.LookupUpdate(pages[i], SlowGetPage);
+        std::cout << cache_hit;
+    #endif
+    
+    #ifdef USE_BELADY
+        CacheBelady<int, int> cache_belady(size_cache, pages);
+        for (size_t i = 0; i < number_of_pages; i++)
+            cache_hit += cache_belady.LookupUpdate(pages[i], SlowGetPage);
+        std::cout << cache_hit;
+    #endif
 
     return 0;
 }
